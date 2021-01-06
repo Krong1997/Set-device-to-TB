c7d2607102aa8ef0fd49e170b3378a5dacd5b217
rong <tairong_kang@innodisk.com>
2021-01-06 16:23:04 +0800

    feat: remove "reset json file"

--- application/reset.js
+++ application/reset.js
@@ -1,18 +1,11 @@
 const showLog = require('../modules/showMsgOnLog');
 const deleteAllDevice = require('../modules/deleteAllDevice');
-const fs = require('fs');
-const { jsonPath } = require('../constant/env');
 
 try {
-  deleteAllDevice().then(() => {
-    showLog('clean device list json file');
-    fs.writeFileSync(jsonPath, '', (err) => {
-      console.error('Data written to file error', err);
-    });
-    showLog('done');
-  });
+  showLog('delete all devices...');
+  deleteAllDevice();
 } catch (error) {
-  
+  console.error('deleteAllDevice error',error);
 }
 
 
--- modules/deleteAllDevice.js
+++ modules/deleteAllDevice.js
@@ -1,5 +1,4 @@
 const proxyToTB = require('./utils/proxyToTB');
-const showLog = require('./showMsgOnLog');
 const {
   host,
   port
@@ -8,7 +7,6 @@ const {
 async function deleteAllDevice() {
   const deviceList = require('../output/deviceList.json');
   if (!Array.isArray(deviceList)) throw new Error('device is not array');
-  showLog('delete all devices...');
   for (let i = 0; i < deviceList.length; i++) {
     try {
       const opt = {
